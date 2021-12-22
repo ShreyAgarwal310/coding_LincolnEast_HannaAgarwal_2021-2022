87f116947c26b913f38e59fad72a032477e4462f
Nixon Hanna <nixonhanna95@gmail.com>
2021-12-22 16:18:26 -0600

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ window.configure(background='white')
 window.title('Thing')
 window.geometry("750x500")
 
-def get_choices():
+def search():
     state = state_dropdown.get()
     state_boolean = not (state == "Select a State")
     city = cities_dropdown.get()
@@ -19,6 +19,18 @@ def get_choices():
     inside = 1 == inside_choice.get()
 
     print(city, state, min_p, max_p, type, rating, inside, state_boolean, city_boolean, type_boolean)
+
+    if (state_boolean and city_boolean and type_boolean):
+        for i in attractions:
+            if(i[1] == city and i[2] == state):
+                in_city.append(i)
+            
+            if(i[1] == city and i[2] == state and i[3] >= min_p and i[3] <= max_p and i[4] == type and i[5] == inside and i[6] >= rating):
+                matches.append(i)
+                print(i)
+
+
+
 
 def pick_cities(e):
     state = state_dropdown.get()
@@ -80,47 +92,10 @@ attractions = [["Golden State Bridge", "San Francisco", "California", 0, "Sights
                 ["Radio City Music Hall", "New York City", "New York", 31, "Pleasure", True, 4.7],
                 ["St. Patrick's Cathedral", "New York City", "New York", 10, "Sightseeing", True, 4.8],
                 ["Carnegie Hall", "New York City", "New York", 300, "Sightseeing", True, 4.7],
-                ["Bryant Park", "New York City", "New York", 0, "Sightseeing", False, 4.7],
-                ["Walt Disney World", "Orlando", "Florida", 110, "Pleasure", False, 4.7],
-                ["Kennedy Space Center", "Orlando", "Florida", 50, "Educational", False, 4],
-                ["Universal Studios", "Orlando", "Florida", 110, "Pleasure", False, 4.7],
-                ["Miami Beach", "Miami", "Florida", 0, "Pleasure", False, 4.4],
-                ["Everglades National Park", "Miami", "Florida", 0, "Nature", False, 4.6],
-                ["Daytona 500 International Speedway", "Orlando", "Florida", 20, "Sightseeing", False, 4.7],
-                ["SeaWorld Orlando", "Orlando", "Florida", 80, "Educational", False, 4.5],
-                ["Busch Gardens Tampa", "Tampa", "Florida", 15, "Pleasure", False, 4.5],
-                ["Duval Street", "Key West", "Florida", 0, "Sightseeing", False, 4.6],
-                ["St. Augustine's Historic District", "Key West", "Flordia", 0, "Sightseeing", False, 1],
-                ["Edison and Ford Winter Estates", "Miami", "Florida", 20, "Sightseeing", True, 4.7],
-                ["Salvador Dali Museum", "Tampa", "Florida", 20, "Educational", True, 4.7],
-                ["Big Bend National Park", "San Antonio", "Texas", 15, "Nature", False, 4.8],
-                ["The Alamo", "San Antonio", "Texas", 0, "Educational", False, 4.6],
-                ["San Antonio's River Walk", "San Antonio", "Texas", 0, "Sightseeing", False, 4.7],
-                ["Space Center Houston", "Houston", "Texas", 30, "Educational", False, 4.6],
-                ["Padre Island National Seashore", "San Antonio", "Texas", 20, "Nature", False, 4.4],
-                ["Texas State Capitol", "Austin", "Texas", 0, "Sightseeing", True, 4.7],
-                ["Sixth Floor Museum", "Dallas", "Texas", 15, "Educational", True, 4.6],
-                ["Fort Worth Stockyards", "Fort Worth", "Texas", 0, "Sightseeing", True, 4.7],
-                ["Galveston Beach", "Houston", "Texas", 10, "Sightseeing", False, 4.5],
-                ["USS Lexington", "San Antonio", "Texas", 20, "Educational", True, 4.8],
-                ["Cadillac Ranch", "Dallas", "Texas", 0, "Sightseeing", False, 4.4],
-                ["Natural Bridge Caverns", "San Antonio", "Texas", 30, "Nature", False, 4.7],
-                ["Houston's Museum District", "Houston", "Texas", 20, "Educational", True, 4.5],
-                ["Gruene Historic District", "San Antonio", "Texas", 10, "Sightseeing", False, 4.3],
-                ["Dallas Arboretum", "Dallas", "Texas", 10, "Sightseeing", False, 4.3],
-                ["Henry Doorly Zoo", "Omaha", "Nebraska", 20, "Educational", False, 4.8],
-                ["Old Market in Omaha", "Omaha", "Nebraska", 0, "Sightseeing", False, 4.4],
-                ["Strategic Air and Space Museum", "Omaha", "Nebraska", 15, "Educational", 4.7],
-                ["Chimney Rock Historic Site", "Scottsbluff", "Nebraska", 0, "Sightseeing", False, 4.2],
-                ["Haymarket District in Lincoln", "Lincoln", "Nebraska", 0, "Sightseeing", False, 4.7],
-                ["Nebraska State Capitol", "Lincoln", "Nebraska", 0, "Sightseeing", True, 4.5],
-                ["Lied Center", "Lincoln", "Nebraska", 50, "Pleasure", True, 4.3],
-                ["Sheldon Museum of Art", "Lincoln", "Nebraska", 0, "Educational", True, 4.5],
-                ["National Museum of Roller Skating", "Lincoln", "Nebraska", 0, "Educational", True, 3.9],
-                ["Scottsbluff National Monument", "Scottsbluff", "Nebraska", 0, "Sightseeing", False, 4.8],
-                ["Golden Spike Tower", "North Platte", "Nebraska", 10, "Sightseeing", False, 4.7],
-                ["Carhenge", "Scottsbluff", "Nebraska", 0, "Sightseeing", False, 4.6]]
+                ["Bryant Park", "New York City", "New York", 0, "Sightseeing", False, 4.7]]
 
+matches = []
+in_city = []
 
 state_choice= StringVar(window)
 city_choice = StringVar(window)
@@ -164,7 +139,7 @@ rating_slider = Scale(window, variable = rating_choice, from_ = 0, to = 5, orien
 rating_slider.place(x = 10, y = 340)
 rating_slider.configure(background = 'white')
 
-search_button = Button(window, text='Search', command=get_choices)
+search_button = Button(window, text='Search', command=search)
 search_button.place(x = 50, y = 400)
 
 
