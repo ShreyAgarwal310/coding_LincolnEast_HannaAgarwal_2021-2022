e2b02d3edfc19bb29b14ad4bec6df128f4aadd7a
Nixon Hanna <nixonhanna95@gmail.com>
2021-12-22 11:22:51 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+import requests
+import json
+import urllib.request
+import shutil
+from tkinter import *
+from tkinter import ttk
+from PIL import ImageTk, Image
+
+
+window = Tk()
+window.configure(background='white')
+window.title('Thing')
+window.geometry("1000x500")
+
+
+separator = ttk.Separator(window, orient='vertical')
+separator.place(relx=0.29, rely=0, relwidth=.001, relheight=1)
+
+
+
+window.mainloop()
