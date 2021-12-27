878c34e8d30362cf9469d58fb4c16190e98765e0
Nixon Hanna <nixonhanna95@gmail.com>
2021-12-27 11:30:47 -0600

    Update main.py

--- main.py
+++ main.py
@@ -55,7 +55,6 @@ def search():
 
                 in_city.append([i, match_counter, non_matches])
             
-            
             if((i[1] == city or (not city_boolean)) and (i[2] == state or (not state_boolean)) and (i[3] <= max_p) and (i[4] == type or (not type_boolean)) and (i[5]) and (i[6] >= rating)):
                 matches.append(i)
     else:
@@ -97,31 +96,87 @@ def search():
 
 
 # changes the possible selections of the city based on which state the user picked
-def pick_cities(e):
+def change_city_dropdown(e):
     state = state_dropdown.get()
-    if(state == "Nebraska"):
-        cities_dropdown['values'] = ne_cities   
-    elif(state == "California"):
-        cities_dropdown['values'] = ca_cities
-    elif(state == "New York"):
-        cities_dropdown['values'] = ny_cities
-    elif(state == "Texas"):
-        cities_dropdown['values'] = tx_cities
-    elif(state == "Florida"):
-        cities_dropdown['values'] = fl_cities
-
+    cities_dropdown['values'] = state_city_dict[state]
     cities_dropdown.configure(state='normal')
     cities_dropdown.set("Select a City")
 
+
+def update_screen(new_screen):
+    if(new_screen + 1 < len(matches)):
+        next_button.place(x=675, y=20)
+    else:
+        next_button.place_forget()
+
+    if(new_screen > 0):
+        back_button.place(x=175, y=20)
+    else:
+        back_button.place_forget()
+
+    title_text.configure(state="normal")
+    title_text.delete("1.0", "end")
+    title_text.insert('end', matches[new_screen][0])
+    title_text.tag_add("center_title", "1.0", "end")
+    title_text.configure(state='disabled')
+    location_text.configure(state="normal")
+    location_text.delete("1.0", "end")
+    location_text.insert('end', f"Location: {matches[new_screen][1]}, {matches[new_screen][2]}")
+    location_text.configure(state='disabled')
+    price_text.configure(state="normal")
+    price_text.delete("1.0", "end")
+    price_text.insert('end', f"Price: {matches[new_screen][3]}$")
+    price_text.configure(state='disabled')
+    type_text.configure(state="normal")
+    type_text.delete("1.0", "end")
+    type_text.insert('end', f"Type: {matches[new_screen][4]}")
+    type_text.configure(state='disabled')
+    indoor_text.configure(state="normal")
+    indoor_text.delete("1.0", "end")
+    indoor_text.insert('end', 'This attraction is indoors') if matches[new_screen][5] else indoor_text.insert('end', 'This attraction is not indoors')
+    indoor_text.configure(state='disabled')
+    rating_text.configure(state="normal")
+    rating_text.delete("1.0", "end")
+    rating_text.insert('end', f"Rating: {matches[new_screen][6]}")
+    rating_text.configure(state='disabled')
+    screenNum_text.configure(state="normal")
+    screenNum_text.delete("1.0", "end")
+    screenNum_text.insert('end', f"{screenNum} / {len(matches)}")
+    screenNum_text.configure(state='disabled')
+
+
+# runs when the next button is pressed to show the next attraction
+def next():
+    # increments screenNum to go to the next screen
+    global screenNum
+    screenNum += 1
+    print("next")
+    # subtract 1 to convert screenNum starting at 1 to an index starting at 0
+    update_screen(screenNum - 1)
+
+
+# runs when the back button is pressed to show the previous attraction
+def back():
+    # increments screenNum down one to go to the previous screen
+    global screenNum
+    screenNum -= 1
+    print("back")
+    # subtract 1 to convert screenNum starting at 1 to an index starting at 0
+    update_screen(screenNum - 1)
+
     
-# initializes all the city and state options for each state
+# initializes all options for state and type
 states_options = ["Nebraska", "California", "New York", "Texas", "Florida"]
-ne_cities = ["Any", "Lincoln", "Omaha", "Scottsbluff", "North Platt"]
-ca_cities = ["Any", "San Francisco", "Los Angeles", "San Jose", "Anaheim"]
-ny_cities = ["Any", "New York City"]
-tx_cities = ["Any", "Houston", "Dallas", "Austin", "San Antonio", "Fort Worth"]
-fl_cities = ["Any", "Miami", "Orlando", "Tampa", "Key West"]
 type_options = ["Any", "Educational", "Sightseeing", "Nature", "Pleasure"]
+
+# dictionary to retrieve cities in each state
+state_city_dict = {
+    "Nebraska": ["Any", "Lincoln", "Omaha", "Scottsbluff", "North Platte"],
+    "California": ["Any", "San Francisco", "Los Angeles", "San Jose", "Anaheim"],
+    "New York": ["Any", "New York City"],
+    "Texas": ["Any", "Houston", "Dallas", "Austin", "San Antonio", "Fort Worth"],
+    "Florida": ["Any", "Miami", "Orlando", "Tampa", "Key West"]
+}
 
 # stores the data for the possible places to go
 attractions = [["Golden Gate Bridge", "San Francisco", "California", 0, "Sightseeing", False, 4.8],
@@ -167,7 +222,7 @@ attractions = [["Golden Gate Bridge", "San Francisco", "California", 0, "Sightse
                 ["SeaWorld Orlando", "Orlando", "Florida", 80, "Educational", False, 4.5],
                 ["Busch Gardens Tampa", "Tampa", "Florida", 15, "Pleasure", False, 4.5],
                 ["Duval Street", "Key West", "Florida", 0, "Sightseeing", False, 4.6],
-                ["St. Augustine's Historic District", "Key West", "Flordia", 0, "Sightseeing", False, 1],
+                ["St. Augustine's Historic District", "Key West", "Florida", 0, "Sightseeing", False, 1],
                 ["Edison and Ford Winter Estates", "Miami", "Florida", 20, "Sightseeing", True, 4.7],
                 ["Salvador Dali Museum", "Tampa", "Florida", 20, "Educational", True, 4.7],
                 ["Big Bend National Park", "San Antonio", "Texas", 15, "Nature", False, 4.8],
@@ -199,89 +254,18 @@ attractions = [["Golden Gate Bridge", "San Francisco", "California", 0, "Sightse
                 ["Carhenge", "Scottsbluff", "Nebraska", 0, "Sightseeing", False, 4.6]]
 
 screenNum = 1
+inside_choice = IntVar()
 
 # creates backup of the attractions list
 attractions_backup = copy.deepcopy(attractions)
 
-def update_screen(new_screen):
-    if(new_screen + 1 < len(matches)):
-        next_button.place(x=675, y=20)
-    else:
-        next_button.place_forget()
-
-    if(new_screen > 0):
-        back_button.place(x=175, y=20)
-    else:
-        back_button.place_forget()
-
-    title_text.configure(state="normal")
-    title_text.delete("1.0", "end")
-    title_text.insert('end', matches[new_screen][0])
-    title_text.tag_add("center_title", "1.0", "end")
-    title_text.configure(state='disabled')
-    location_text.configure(state="normal")
-    location_text.delete("1.0", "end")
-    location_text.insert('end', f"Location: {matches[new_screen][1]}, {matches[new_screen][2]}")
-    location_text.configure(state='disabled')
-    price_text.configure(state="normal")
-    price_text.delete("1.0", "end")
-    price_text.insert('end', f"Price: {matches[new_screen][3]}")
-    price_text.configure(state='disabled')
-    type_text.configure(state="normal")
-    type_text.delete("1.0", "end")
-    type_text.insert('end', f"Type: {matches[new_screen][4]}")
-    type_text.configure(state='disabled')
-    indoor_text.configure(state="normal")
-    indoor_text.delete("1.0", "end")
-    if matches[new_screen][5]:  
-        indoor_text.insert('end', 'This attraction is indoors')
-    else:
-        indoor_text.insert('end', 'This attraction is not indoors')
-    indoor_text.configure(state='disabled')
-    rating_text.configure(state="normal")
-    rating_text.delete("1.0", "end")
-    rating_text.insert('end', f"Rating: {matches[new_screen][6]}")
-    rating_text.configure(state='disabled')
-    screenNum_text.configure(state="normal")
-    screenNum_text.delete("1.0", "end")
-    screenNum_text.insert('end', f"{screenNum} / {len(matches)}")
-    screenNum_text.configure(state='disabled')
-
-
-# runs when the next button is pressed to show the next attraction
-def next():
-    # increments screenNum to go to the next screen
-    global screenNum
-    screenNum += 1
-    print("next")
-    # subtract 1 to convert screenNum starting at 1 to an index starting at 0
-    update_screen(screenNum - 1)
-
-
-# runs when the back button is pressed to show the previous attraction
-def back():
-    # increments screenNum down one to go to the previous screen
-    global screenNum
-    screenNum -= 1
-    print("back")
-    # subtract 1 to convert screenNum starting at 1 to an index starting at 0
-    update_screen(screenNum - 1)
-
-
-# initializes variables to store user's input
-state_choice= StringVar(window)
-city_choice = StringVar(window)
-max_price = DoubleVar(window)
-type_choice = StringVar(window)
-rating_choice = DoubleVar(window)
-inside_choice = IntVar()
-
 # creates the dropdown where users select their state
-state_dropdown = ttk.Combobox(window, width=16, textvariable=state_choice)
+state_dropdown = ttk.Combobox(window, width=16)
+
 state_dropdown['values'] = states_options
 state_dropdown.set("Select a State")
 state_dropdown.place(x=10, y=70)  
-state_dropdown.bind("<<ComboboxSelected>>", pick_cities)
+state_dropdown.bind("<<ComboboxSelected>>", change_city_dropdown)
 
 # creates the dropdown where users select their city
 cities_dropdown = ttk.Combobox(window, width=16, values=["Select a State First"])
@@ -294,12 +278,13 @@ type_dropdown = ttk.Combobox(window, width=16, value=type_options)
 type_dropdown.set("Select a Type")
 type_dropdown.place(x=10, y=150)
 
-# creates the dropdown where users select wheter or not they want to be outside
+# creates the dropdown where users select whether or not they want to be outside
 inside_check = Checkbutton(window, text="Inside Only", variable=inside_choice, background='white')
 inside_check.place(x=10, y=190)
 
 # creates the slider where users decide the maximum price of their attraction
-max_price_slider = Scale(window, variable=max_price, from_=0, to=300, orient=HORIZONTAL, resolution=5)
+max_price_slider = Scale(window, from_=0, to=300, orient=HORIZONTAL, resolution=5)
+
 max_price_slider.place(x=10, y=250)
 max_price_slider.set(300)
 max_price_slider.configure(background='white')
@@ -310,7 +295,8 @@ max_text.insert('end', 'Max Price')
 max_text.configure(state='disabled')
 
 # creates the slider where users decide the rating they want their attraction to be
-rating_slider = Scale(window, variable=rating_choice, from_=0, to=5, orient=HORIZONTAL, resolution=0.1)
+rating_slider = Scale(window, from_=0, to=5, orient=HORIZONTAL, resolution=0.1)
+
 rating_slider.place(x=10, y=320)
 rating_slider.configure(background='white')
 
@@ -364,5 +350,4 @@ screenNum_text.configure(state='disabled')
 
 
 window.mainloop()
-
 
