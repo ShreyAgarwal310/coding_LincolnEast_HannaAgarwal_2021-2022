cb01f1a546c15a3c047b7c445546b91d7a64d8a5
Nixon Hanna <nixonhanna95@gmail.com>
2021-12-31 16:47:06 -0600

    Update drawing_demo.py

--- drawing_demo.py
+++ drawing_demo.py
@@ -35,11 +35,18 @@ class ExampleApp(tk.Tk):
 
         self.canvas.bind("<B1-Motion>", self.draw_from_where_you_are)
 
+        self.canvas.bind("<ButtonRelease>", self.reset_square)
+
+    def reset_square(self, event):
+        global initial_x, initial_y, final_x, final_y
+        print(initial_x, initial_y, final_x, final_y)
+        self.points_recorded[:] = []
+        print(initial_x, initial_y, final_x, final_y)
+
     def clear_all(self):
-        global closed_points
         self.canvas.delete("all")
-        self.points_recorded[:] = closed_points
-        self.canvas.create_line(self.points_recorded, fill="yellow")
+
+        self.points_recorded[:] = []
 
     def print_points(self):
 
@@ -47,7 +54,6 @@ class ExampleApp(tk.Tk):
             self.points_recorded.pop()
             self.points_recorded.pop()
 
-        self.canvas.create_line(self.points_recorded, fill="yellow")
         print(self.points_recorded)
 
         self.points_recorded[:] = []
@@ -58,41 +64,41 @@ class ExampleApp(tk.Tk):
         self.previous_y = event.y
 
     def draw_from_where_you_are(self, event):
-        global closed_points
+        global initial_x, initial_y, final_x, final_y
         if self.points_recorded:
             self.points_recorded.pop()
             self.points_recorded.pop()
-
-        if ([self.previous_x, self.previous_y] in self.points_recorded):
-            closed_index_initial = self.points_recorded.index(
-                [self.previous_x, self.previous_y])
-            closed_points = self.points_recorded[closed_index_initial + 1:]
-            print(
-                f"closed: {self.previous_x}, {self.previous_y}, {closed_index_initial}")
-            closed_points.append([self.previous_x, self.previous_y])
-
-            # print(closed_points)
-            # closed_index_final = closed_points.index(
-            #     [self.previous_x, self.previous_y])
-            # closed_points = closed_points[:closed_index_final]
-            # print(closed_points)
 
         self.x = event.x
 
         self.y = event.y
 
-        self.canvas.create_line(self.previous_x, self.previous_y,
-
-                                self.x, self.y, fill="yellow")
-
         self.points_recorded.append([self.previous_x, self.previous_y])
 
-        # self.points_recorded.append(self.previous_y)
         self.points_recorded.append(self.x)
         self.points_recorded.append(self.x)
         self.previous_x = self.x
 
         self.previous_y = self.y
+
+        self.canvas.delete("all")
+
+        self.canvas.create_line(self.points_recorded[0][0], self.points_recorded[0][1],
+                                self.points_recorded[0][0], self.y, fill="yellow")
+
+        self.canvas.create_line(self.points_recorded[0][0], self.points_recorded[0][1],
+                                self.x, self.points_recorded[0][1], fill="yellow")
+
+        self.canvas.create_line(self.x, self.points_recorded[0][1],
+                                self.x, self.y, fill="yellow")
+
+        self.canvas.create_line(self.points_recorded[0][0], self.y,
+                                self.x, self.y, fill="yellow")
+
+        initial_x = self.points_recorded[0][0]
+        initial_y = self.points_recorded[0][1]
+        final_x = self.x
+        final_y = self.y
 
 
 if __name__ == "__main__":
