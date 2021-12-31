b4cd67b54df60d9040677e3790570d4e8abd5bdb
Nixon Hanna <nixonhanna95@gmail.com>
2021-12-31 16:49:45 -0600

    Update drawing_demo.py

--- drawing_demo.py
+++ drawing_demo.py
@@ -4,37 +4,21 @@ import tkinter as tk
 class ExampleApp(tk.Tk):
 
     def __init__(self):
-
         tk.Tk.__init__(self)
-
         self.previous_x = self.previous_y = 0
-
         self.x = self.y = 0
-
         self.points_recorded = []
-
         self.canvas = tk.Canvas(
-
             self, width=400, height=400, bg="black", cursor="cross")
-
         self.canvas.pack(side="top", fill="both", expand=True)
-
         self.button_print = tk.Button(
-
             self, text="Display points", command=self.print_points)
-
         self.button_print.pack(side="top", fill="both", expand=True)
-
         self.button_clear = tk.Button(
-
             self, text="Clear", command=self.clear_all)
-
         self.button_clear.pack(side="top", fill="both", expand=True)
-
         self.canvas.bind("<Motion>", self.tell_me_where_you_are)
-
         self.canvas.bind("<B1-Motion>", self.draw_from_where_you_are)
-
         self.canvas.bind("<ButtonRelease>", self.reset_square)
 
     def reset_square(self, event):
@@ -45,22 +29,17 @@ class ExampleApp(tk.Tk):
 
     def clear_all(self):
         self.canvas.delete("all")
-
         self.points_recorded[:] = []
 
     def print_points(self):
-
         if self.points_recorded:
             self.points_recorded.pop()
             self.points_recorded.pop()
-
         print(self.points_recorded)
-
         self.points_recorded[:] = []
 
     def tell_me_where_you_are(self, event):
         self.previous_x = event.x
-
         self.previous_y = event.y
 
     def draw_from_where_you_are(self, event):
@@ -68,33 +47,22 @@ class ExampleApp(tk.Tk):
         if self.points_recorded:
             self.points_recorded.pop()
             self.points_recorded.pop()
-
         self.x = event.x
-
         self.y = event.y
-
         self.points_recorded.append([self.previous_x, self.previous_y])
-
         self.points_recorded.append(self.x)
         self.points_recorded.append(self.x)
         self.previous_x = self.x
-
         self.previous_y = self.y
-
         self.canvas.delete("all")
-
         self.canvas.create_line(self.points_recorded[0][0], self.points_recorded[0][1],
                                 self.points_recorded[0][0], self.y, fill="yellow")
-
         self.canvas.create_line(self.points_recorded[0][0], self.points_recorded[0][1],
                                 self.x, self.points_recorded[0][1], fill="yellow")
-
         self.canvas.create_line(self.x, self.points_recorded[0][1],
                                 self.x, self.y, fill="yellow")
-
         self.canvas.create_line(self.points_recorded[0][0], self.y,
                                 self.x, self.y, fill="yellow")
-
         initial_x = self.points_recorded[0][0]
         initial_y = self.points_recorded[0][1]
         final_x = self.x
@@ -102,7 +70,5 @@ class ExampleApp(tk.Tk):
 
 
 if __name__ == "__main__":
-
     app = ExampleApp()
-
     app.mainloop()
